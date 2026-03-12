b1688b757366385c8a43cb7bb0febee9032918c0
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-03-12 18:44:45 +0100

    chore: prepare web app for cloudflare workers deploy

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { escapeSvelte, mdsvex } from 'mdsvex';
 import { createHighlighter } from 'shiki';
@@ -146,9 +146,6 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
